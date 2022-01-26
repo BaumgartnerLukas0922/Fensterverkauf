612071bad731ea0bcd77562d939b1a84a8994c5e
AbazovicEdinaIF <edina15042305@gmail.com>
2022-01-26 14:44:06 +0100

    add enums

---
+++ src/main/java/at/htl/fensterverkauf/Material.java
@@ -0,0 +1,7 @@
+package at.htl.fensterverkauf;
+
+public enum Material {
+    HolzAlu,
+    KunststoffAlu,
+    Kunststoff
+}
---
+++ src/main/java/at/htl/fensterverkauf/Properties.java
@@ -0,0 +1,7 @@
+package at.htl.fensterverkauf;
+
+public enum Properties {
+    Panzerglas,
+    Milchglas,
+    UVSchutz
+}
