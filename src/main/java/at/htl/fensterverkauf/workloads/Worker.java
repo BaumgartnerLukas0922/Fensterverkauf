8d9c8b24013bc088acea1b7e7b9e72f3e0eb2952
Lukas Baumgartner <euklas7@gmail.com>
2022-02-01 13:20:42 +0100

    Added Window List to Worker

--- src/main/java/at/htl/fensterverkauf/workloads/Worker.java
+++ src/main/java/at/htl/fensterverkauf/workloads/Worker.java
@@ -1,9 +1,8 @@
 package at.htl.fensterverkauf.workloads;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Worker extends Employee{
@@ -11,11 +10,11 @@ public class Worker extends Employee{
     @Id
     int WorkerId;
 
-    @ManyToOne
-    Window windows; //should be a List
+    @OneToMany
+    List<Window> windows; //should be a List
 
     //region Constructor
-    public Worker(String firstName, String lastName, Double salary, int hours, Window windows) {
+    public Worker(String firstName, String lastName, Double salary, int hours, List<Window> windows) {
         super(firstName, lastName, salary, hours);
         this.windows = windows;
     }
