10919c4e9b0f28d9cf7a195960cd53d7fee0e95c
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-01 14:34:29 +0100

    added-first-resource-class

---
+++ src/main/java/at/htl/fensterverkauf/api/FensterVerkaufResource.java
@@ -0,0 +1,17 @@
+package at.htl.fensterverkauf.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/verkauf")
+public class FensterVerkaufResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Herzlich willkommen zu unserem Fensterverkauf!";
+    }
+
+}
