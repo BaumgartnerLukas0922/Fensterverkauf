27bffa31e6bafe120379327f1893f8b967ffdc8d
AbazovicEdinaIF <edina15042305@gmail.com>
2022-02-01 15:22:02 +0100

    create tests for entities

---
+++ src/main/test/at/htl/fensterverkauf/workloads/EmployeeTest.java
@@ -0,0 +1,16 @@
+package at.htl.fensterverkauf.workloads;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class EmployeeTest {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
