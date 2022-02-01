40fb6486dbf794ee65151f4f39b1bb92ca694391
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-01 23:37:41 +0100

    Qute-Resource-etc

--- src/main/java/at/htl/fensterverkauf/api/CommissionResource.java
+++ src/main/java/at/htl/fensterverkauf/api/CommissionResource.java
@@ -1,12 +1,21 @@
 package at.htl.fensterverkauf.api;
 
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/verkauf")
-public class FensterVerkaufResource {
+@Path("/comission")
+public class ComissionResource {
+
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance fensterverkauf(String name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
