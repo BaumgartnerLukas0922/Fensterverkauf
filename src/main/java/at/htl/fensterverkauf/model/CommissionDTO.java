e0157a1f378eab3b5deb4dd9fc35a7fbcd3ba4c7
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-02 13:14:33 +0100

    #6-commissionDTO

---
+++ src/main/java/at/htl/fensterverkauf/model/CommissionDTO.java
@@ -0,0 +1,35 @@
+package at.htl.fensterverkauf.model;
+
+import at.htl.fensterverkauf.workloads.Location;
+import at.htl.fensterverkauf.workloads.Shipment;
+
+
+public class CommissionDTO {
+
+    private Shipment shipment;
+    private Location location;
+
+    public CommissionDTO() {
+    }
+
+    public CommissionDTO(Shipment shipment, Location location) {
+        this.shipment = shipment;
+        this.location = location;
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
