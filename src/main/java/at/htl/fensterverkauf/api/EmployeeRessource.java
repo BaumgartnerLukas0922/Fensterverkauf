fd5deb5432e27da207724d32814960c78d58af2f
oliversugic <osugic@gmail.com>
2022-02-07 21:02:57 +0100

    i dont know what wos going to be

--- src/main/java/at/htl/fensterverkauf/api/EmployeeRessource.java
+++ src/main/java/at/htl/fensterverkauf/api/EmployeeRessource.java
@@ -7,8 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
-@Produces("application/json")
-@Consumes("application/json")
+
 @Path("employee")
 public class EmployeeRessource {
     //private final EmployeeService employeeService;
